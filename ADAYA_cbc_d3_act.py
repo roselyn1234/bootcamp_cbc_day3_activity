b7363278d8eaa483c77f79386b4dc5cc4f8404c8
roselyn1234 <roselyn.adaya@aclcbutuan.edu.ph>
2025-03-01 09:32:53 +0800

    Add files via upload

---
+++ ADAYA_cbc_d3_act.py
@@ -0,0 +1,16 @@
+text= "hello world"
+capitalized_text = text.capitalize()
+print ("capitalized:", capitalized_text)
+
+centered_text = text.center(13, "_")
+print ("centered:", centered_text)
+
+count_1 = text.count("1")
+print ("count of '1':", count_1) 
+
+alnum_check = text. replace(" ", "").isalnum()
+print ("Is Alphanumeric:", alnum_check)
+
+index_o = text.index("o")
+print("index of 'o':", index_o)
+
